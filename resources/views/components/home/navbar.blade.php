40b5c46146465417147849b43afe17bdcf7b6c2c
Priskila <priskila2703@gmail.com>
2024-06-11 23:16:40 +0700

    changing few navbar design and backend

--- resources/views/components/home/navbar.blade.php
+++ resources/views/components/home/navbar.blade.php
@@ -1,30 +1,34 @@
-<nav class="navbar navbar-expand-lg py-4 border-bottom">
-    <div class="container">
-        <a class="navbar-brand fw-bold" href="#">Pet Shop</a>
+<nav class="navbar navbar-expand-lg">
+    <div class="container border-bottom py-3">
+        <a class="navbar-brand fw-bold d-flex align-items-center gap-2 text-pink" href=".">
+            <img src="assets2/images/logo.png" width="40" alt="Logo" />
+            Pet Shop
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarMenu">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarMenu">
-            <ul class="navbar-nav ms-auto">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 gap-2">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href=".">Home</a>
+                    <a class="nav-link {{ request()->is('/') ? 'active' : '' }}" aria-current="page"
+                        href=".">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('about') }}">About Us</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Gallery</a>
+                    <a class="nav-link {{ request()->routeIs('about') ? 'active' : '' }}"
+                        href="{{ route('about') }}">About Us</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">Shop</a>
                 </li>
                 <li class="nav-item">
-                    <a class="ms-4 btn btn-utama" href="{{ route('reservation_page') }}">Reservation</a>
+                    <a class="btn btn-primary px-4" href="{{ route('reservation_page') }}">
+                        <i class="bx bx-calendar"></i>Reservation
+                    </a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
                         aria-expanded="false">
-                        Hai, {{ Auth::user()->name }}
+                        Hi, {{ Auth::user()->name }}
                     </a>
                     <ul class="dropdown-menu">
                         <li>
@@ -48,3 +52,6 @@
         </div>
     </div>
 </nav>
+
+
+<script src="assets/vendors/bootstrap/js/bootstrap.bundle.min.js"></script>
