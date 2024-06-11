106e57f9461ae6ed842ac1ca0663c86ffdf236b6
Priskila <priskila2703@gmail.com>
2024-06-11 22:07:07 +0700

    adding css and js link to homepage

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -2,10 +2,13 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>{{ $title }}</title>
 
+    <link rel="stylesheet" href="assets/vendors/bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="assets/vendors/boxicons/css/boxicons.min.css" />
+    <link rel="stylesheet" href="assets/css/style.css" />
     @include('components.style')
 </head>
 
