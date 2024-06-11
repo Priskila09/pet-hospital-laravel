34974ab9710c9d03a49491c14a31171e76ea256a
Priskila <priskila2703@gmail.com>
2024-06-11 21:30:01 +0700

    adding css and bootstrap to resources

--- resources/views/components/style.blade.php
+++ resources/views/components/style.blade.php
@@ -1,3 +1,5 @@
 <link rel="stylesheet" href="{{ url('assets/vendors/bootstrap/css/bootstrap.min.css') }}">
 <link rel="stylesheet" href="{{ url('assets/vendors/boxicons/css/boxicons.min.css') }}">
+<script src="{{ url('assets/vendors/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <link rel="stylesheet" href="{{ url('assets/css/style.css') }}">
+<link rel="stylesheet" href="{{ url('assets2/css/style.css') }}">
